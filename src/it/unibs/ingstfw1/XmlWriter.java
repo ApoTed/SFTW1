73acc6a6d36b5d754eaf98207cfc610230b26334
ApoTed <j.tedeschi@studenti.unibs.it>
2022-03-15 20:22:34 +0100

    visualizzazione beta e inserimento num minimo sottocategorie fatto

---
+++ src/it/unibs/ingstfw1/XmlWriter.java
@@ -0,0 +1,126 @@
+package it.unibs.ingstfw1;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class XmlWriter {
+    public static void reader(String filename, Gerarchia ramo){
+        XMLOutputFactory xmlof = null;
+        XMLStreamWriter xmlw = null;
+        try {
+            xmlof = XMLOutputFactory.newInstance();
+            xmlw = xmlof.createXMLStreamWriter(new FileOutputStream(filename), "utf-8");
+            xmlw.writeStartDocument("utf-8", "1.0");
+        } catch (Exception e) {
+            System.out.println("Errore nell'inizializzazione del writer:");
+            System.out.println(e.getMessage());
+        }
+        String[] autori = {"Roberto", "Jacopo", "Enrico", "Stefano"}; // esempio di dati da scrivere
+
+        try { // blocco try per raccogliere eccezioni
+            xmlw.writeStartElement("programmaArnaldo"); // scrittura del tag radice <programmaArnaldo>
+            xmlw.writeComment("INIZIO LISTA"); // scrittura di un commento
+            for (int i = 0; i < autori.length; i++) {
+                xmlw.writeStartElement("autore"); // scrittura del tag autore...
+                xmlw.writeAttribute("id", Integer.toString(i)); // ...con attributo id...
+                xmlw.writeCharacters(autori[i]); // ...e content dato
+                xmlw.writeEndElement(); // chiusura di </autore>
+            }
+            xmlw.writeEndElement(); // chiusura di </programmaArnaldo>
+            xmlw.writeEndDocument(); // scrittura della fine del documento
+            xmlw.flush(); // svuota il buffer e procede alla scrittura
+            xmlw.close(); // chiusura del documento e delle risorse impiegate
+        } catch (Exception e) { // se c’è un errore viene eseguita questa parte
+            System.out.println("Errore");
+        }
+        try{
+            xmlw.writeStartElement("");
+
+        }catch (Exception e){
+
+        }
+    }
+    public static void domWriter(){
+        String xmlFilePath = "C:\\Users\\apote\\Desktop\\Dom\\testX.xml";
+        try {
+
+            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+
+            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+
+            Document document = documentBuilder.newDocument();
+
+            // root element
+            Element root = document.createElement("company");
+            document.appendChild(root);
+
+            // employee element
+            Element employee = document.createElement("employee");
+
+            root.appendChild(employee);
+
+            // set an attribute to staff element
+            Attr attr = document.createAttribute("id");
+            attr.setValue("10");
+            employee.setAttributeNode(attr);
+
+            //you can also use staff.setAttribute("id", "1") for this
+
+            // firstname element
+            Element firstName = document.createElement("firstname");
+            firstName.appendChild(document.createTextNode("James"));
+            employee.appendChild(firstName);
+
+            // lastname element
+            Element lastname = document.createElement("lastname");
+            lastname.appendChild(document.createTextNode("Harley"));
+            employee.appendChild(lastname);
+
+            // email element
+            Element email = document.createElement("email");
+            email.appendChild(document.createTextNode("james@example.org"));
+            employee.appendChild(email);
+
+            // department elements
+            Element department = document.createElement("department");
+            department.appendChild(document.createTextNode("Human Resources"));
+            employee.appendChild(department);
+
+            // create the xml file
+            //transform the DOM Object to an XML File
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource domSource = new DOMSource(document);
+            StreamResult streamResult = new StreamResult(new File(xmlFilePath));
+
+            // If you use
+            // StreamResult result = new StreamResult(System.out);
+            // the output will be pushed to the standard output ...
+            // You can use that for debugging
+
+            transformer.transform(domSource, streamResult);
+
+            System.out.println("Done creating XML File");
+
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (TransformerException tfe) {
+            tfe.printStackTrace();
+        }
+
+    }
+}
--- src/it/unibs/ingstfw1/main.java
+++ src/it/unibs/ingstfw1/main.java
@@ -1,6 +1,9 @@
 package it.unibs.ingstfw1;
 
+import jdk.management.jfr.FlightRecorderMXBean;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class main {
 
@@ -12,15 +15,38 @@ public class main {
 		DatiUtenti x=new DatiUtenti(l, c);
 		x.addUtente("primo", "12", true);
 		//Utilita.menuAccesso(x);
-		//CampoNativo primo=new CampoNativo("stato di conservazione","",true);
-		//CampoNativo primos=new CampoNativo("descrizione libera","",false);
-		//ArrayList <CampoNativo> campiIniziali=new ArrayList<>();
-		//campiIniziali.add(primo);
-		//campiIniziali.add(primos);
-		Gerarchia testGer=Gerarchia.creaRamo();
-		System.out.println(testGer.vediRamo());
+		CampoNativo primo=new CampoNativo("stato di conservazione","",true);
+		CampoNativo primos=new CampoNativo("descrizione libera","",false);
+		ArrayList <CampoNativo> campiIniziali=new ArrayList<>();
+		campiIniziali.add(primo);
+		campiIniziali.add(primos);
+		//Gerarchia testGer=Gerarchia.creaRamo();
+		//System.out.println(testGer.vediRamo());
 		//Categoria testCat=Categoria.creaCategoria(campiIniziali);
-	//System.out.println(testCat.toStringCategoria());
+		//System.out.println(testCat.toStringCategoria());
+		Categoria radice=new Categoria("libro", "opera cartacea",campiIniziali);
+		Categoria romanzo=new Categoria("romanzo", "opera cartacea storia finta",campiIniziali);
+		Categoria giallo=new Categoria("giallo", "opera cartacea detective",campiIniziali);
+		Categoria fantoccio=new Categoria("inesistente","",campiIniziali);
+		ArrayList<CampoNativo> C2= campiIniziali;
+		CampoNativo appiu=new CampoNativo("lunghezza","",true);
+		C2.add(appiu);
+		Categoria fiaba=new Categoria("fiaba", "opera cartacea per bimbi",C2);
+		Categoria bimbi=new Categoria("bimbi", "per bimbi",C2);
+		Categoria disegno=new Categoria("disegno0", "da disegnare",C2);
+		Categoria romantico=new Categoria("romantico", "donne",campiIniziali);
+		HashMap <Categoria, Categoria> toto=new HashMap<>();
+		toto.put(radice, fantoccio);
+		toto.put(romanzo, radice);
+		toto.put(giallo, romanzo);
+		toto.put(romantico,romanzo);
+		toto.put(fiaba, bimbi);
+		toto.put(bimbi, radice);
+		toto.put(disegno, bimbi);
+		Gerarchia g=new Gerarchia(toto, radice);
+		XmlWriter.domWriter();
+
+		XmlWriter.reader("test", g);
 	}
 
 }
